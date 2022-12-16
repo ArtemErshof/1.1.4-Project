d33cdbda63d2e143bd2a30cedb915e365d9672cf
ArtemErshov <artem8ka@yandex.ru>
2022-12-16 21:02:31 +0300

    1.1.4: Realization

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private final static String URL = "jdbc:mysql://localhost:3306/new_schema";
+    private final static String USER = "root";
+    private final static String PASSWORD = "b0gs1LLa!";
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Соединение успешно.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Соединение не установленно.");
+        }
+        return connection;
+    }
 }
+
+
+
